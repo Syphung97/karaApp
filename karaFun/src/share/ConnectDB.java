124be4fe46057dcaa16d582ffd76492524864ecf
Syphung97 <syphung97@gmail.com>
2018-11-10 11:07:08 +0700

    init

---
+++ karaFun/src/share/ConnectDB.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package share;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Sy Phung
+ */
+public class ConnectDB {
+
+}
