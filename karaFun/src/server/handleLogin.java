ebdb843d7d7902aa0affcc311e25d4d5b0fa2341
Syphung97 <syphung97@gmail.com>
2018-11-11 10:08:09 +0700

    Login Handle

---
+++ karaFun/src/server/handleLogin.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Account;
+import server.DAO.AccountDAO;
+
+/**
+ *
+ * @author Sy Phung
+ */
+public class handleLogin extends Thread{
+    Socket socket;
+
+    public handleLogin(Socket s) throws IOException {
+        this.socket = s;
+    }
+
+    @Override
+    public void run() {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            Account a = (Account) ois.readObject();
+            System.out.println(a.getUsername());
+            Account a1 = new AccountDAO().getAccount(a.getUsername());
+            if(a.equals(a1)){
+               oos.writeObject(a1); 
+            }        
+            else{
+                oos.writeObject(null);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(handleLogin.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(handleLogin.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(handleLogin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ServerControl.remove(this);
+    }
+    
+    
+    
+}
